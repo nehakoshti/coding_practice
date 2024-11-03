f455a035b764c220c908d051ab5f9a62b45891a8
nehakoshti <72657711+nehakoshti@users.noreply.github.com>
2024-11-03 19:27:13 +0530

    Update 1.countPairsWithDiffK.py

--- 1.countPairsWithDiffK.py
+++ 1.countPairsWithDiffK.py
@@ -1,12 +1,13 @@
-# Given an array arr[] of positive integers. Find the number of pairs of integers whose difference equals a given number k.
-# Note: (a, b) and (b, a) are considered the same. Also, the same numbers at different indices are considered different.
+# https://www.geeksforgeeks.org/problems/pairs-with-difference-k1713/1
+"""Given an array arr[] of positive integers. Find the number of pairs of integers whose difference equals a given number k.
+Note: (a, b) and (b, a) are considered the same. Also, the same numbers at different indices are considered different.
 
-# Examples:
+Examples:
 
-# Input: arr[] = [1, 5, 3, 4, 2], k = 3
-# Output: 2
-# Explanation: There are 2 pairs with difference 3,the pairs are {1, 4} and {5, 2} 
-
+Input: arr[] = [1, 5, 3, 4, 2], k = 3
+Output: 2
+Explanation: There are 2 pairs with difference 3,the pairs are {1, 4} and {5, 2} 
+"""
 def countPairsWithDiffK(self,arr, k):
     occ=[0]*(max(arr)+1)
     count=0
